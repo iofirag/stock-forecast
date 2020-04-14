b8ccf222abeec7374d464797c23f1b39aa974162
iofirag <iofirag@gmail.com>
2020-04-14 13:45:02 +0300

    change output json

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import investController
-import json
 import utils
+import simplejson
 
 def main():
     result = investController.investigateTickers()
     with open('data.json', 'w') as outfile:
-        json.dump(result, outfile, default=utils.convert)
+        simplejson.dump(result, outfile, default=utils.convert, sort_keys=True, ignore_nan=True)
 
 if __name__ == "__main__":
     main()
